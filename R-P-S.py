c098bd69d7d1af6932c2c8456bd323694f1e5851
Javad Hamidi <tatdesign96@gmail.com>
2022-10-09 18:27:56 +0330

    I just Finished the R-P-S.py

--- R-P-S.py
+++ R-P-S.py
@@ -2,5 +2,25 @@ import random
 
 # Just published On GIT
 
-user_choice = input("[R] for Rock , [P] for Paper , [S] for scissors")
+user_choice = input("What is Your Choice ?[R] for Rock , [P] for Paper , [S] for scissors: \n")
 pc_choice = random.choice(["R", "P", "S"])
+
+
+def is_win():
+    if user_choice == "R" and pc_choice == "S" or user_choice == "P" and pc_choice == "R" or user_choice == "S" and \
+            pc_choice == "S":
+        return True
+
+
+def rpc():
+    if user_choice == pc_choice:
+        print("Play Again , it was a tie")
+    # r>s p>r s>p
+    if is_win():
+        print("You Just Won")
+
+    print("You Lose !")
+
+
+print(pc_choice)
+rpc()
